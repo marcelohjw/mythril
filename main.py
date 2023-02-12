69b406db363dc49888fb980fc6c22cd1187b6f98
Marcelo Junior <marcelohjw@gmail.com>
2023-02-12 05:44:16 -0300

    Storyline added to mythril

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print("=========================================================================
 print("Bem vindo ao mundo de " + cores.UNDERLINE + cores.FAIL + "Mythril" + cores.ENDC)
 print("==========================================================================================")
 print("Cuidado com os " + cores.FAIL + "ladrões" + cores.ENDC + " e " + cores.WARNING + "ratos" + "!" + cores.ENDC)
-print("Ganhe um " + cores.FAIL + "confronto" + cores.ENDC + " para vencer o jogo!")
+print("Obtenha o famoso " + cores.OKGREEN + "Ark" + cores.ENDC + " para vencer o jogo!")
 
 if os.path.isfile("./save.txt") and os.stat("./save.txt").st_size != 0:
     save_antigo = open("./save.txt", "r+")
@@ -20,5 +20,5 @@ if os.path.isfile("./save.txt") and os.stat("./save.txt").st_size != 0:
     else:
         from tree import rootone
 else:
-    print(cores.FAIL + "Nome antigo nao encontrado..." + cores.ENDC)
-    from tree import rootone
+    print(cores.FAIL + "Nome antigo não encontrado..." + cores.ENDC)
+    from story import storyline
---
+++ story/storyline.py
@@ -0,0 +1,9 @@
+from classes.config import cores
+
+print("------------------------------------------------------------------------------------------")
+print(cores.HEADER +  "Era uma vez uma terra carmesim com rubis que emanavm do chão, muito valiosa, seu nome era Mythril..." + cores.ENDC)
+print(cores.HEADER +  "Muitos se aventuram a vasculha-la porém, não imaginam as criaturas que podem encontrar em sua jornada" + cores.ENDC)
+print(cores.HEADER +  "Meu avô chegou a comentar que nunca alguém conseguiu obter o tesouro mais valioso de Mythril" + cores.ENDC)
+print(cores.HEADER +  "O famoso" + cores.OKGREEN +  " Ark"+ cores.ENDC)
+print(cores.HEADER +  "Será que vossa senhoria conseguirá realizar tal feito? Tem coragem?" + cores.ENDC)
+from tree import rootone
